ca0e2fe43f5de6718b2fa0f7433d42f89373da22
Kshitij9921 <kshitijcobra@gmail.com>
2022-01-25 09:48:36 +0530

    Camera position, Player Control
    Camera Set as first person perspective, and made abasic Player Controller

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,14 +7,17 @@ public class CharacterController : MonoBehaviour
     public float playerSpeed;
     public float playerJumpForce;
     private Rigidbody rb;
+    private bool isGrounded;
+    private CapsuleCollider capsuleCollider;
     // Start is called before the first frame update
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
     }
     void Start()
     {
-        
+
     }
     private void FixedUpdate()
     {
@@ -26,12 +29,30 @@ public class CharacterController : MonoBehaviour
     void Update()
     {
 
-        
 
+
+    }
+
+    bool PlayerGrounded()
+    {
+        RaycastHit hitInfo;
+
+        if (Physics.SphereCast(transform.position, capsuleCollider.radius, Vector3.down, out hitInfo, ((capsuleCollider.height / 2.0f) - capsuleCollider.radius + 0.1f)))
+        {
+            //Debug.DrawRay(transform.position, hitInfo.point, Color.yellow);
+            print("True!!");
+            return true;
+        }
+        else
+        {
+            print("False");
+            return false;
+        }
     }
 
     void PlayerMovement()
     {
+
         float horizontalMovement = Input.GetAxis("Horizontal") * playerSpeed;
         float forwardMovement = Input.GetAxis("Vertical") * playerSpeed;
 
@@ -42,10 +63,76 @@ public class CharacterController : MonoBehaviour
 
     void PlayerJumpMovement()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && PlayerGrounded())
         {
+            print("JUMP!!");
             rb.AddForce(0, playerJumpForce, 0);
+            //rb.velocity = new Vector3(0,playerJumpForce,0);
         }
-        
+
     }
+
+
+
+    //public float playerSpeed;
+    //Rigidbody rb;
+    //public float playerJumpValue;
+    //private bool isGrounded;
+    //private CapsuleCollider capsuleCollider;
+
+    //private void Awake()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //    capsuleCollider = GetComponent<CapsuleCollider>();
+    //}
+
+    //void Start()
+    //{
+
+    //}
+    //void Update()
+    //{
+
+    //}
+
+    //void FixedUpdate()
+    //{
+    //    PlayerMovement();
+    //    PlayerJumpMovement();
+    //}
+
+    //bool PlayerGrounded()
+    //{
+    //    RaycastHit hitInfo;
+
+    //    if (Physics.SphereCast(transform.position, capsuleCollider.radius, Vector3.down, out hitInfo, (capsuleCollider.height / 2.0f) - capsuleCollider.radius + 0.1f))
+    //    {
+    //        print("true statement");
+    //        return true;
+
+    //    }
+    //    else
+    //    {
+    //        print("false statement");
+    //        return false;
+    //    }
+    //}
+
+    //void PlayerMovement()
+    //{
+    //    float horizontalMovement = Input.GetAxis("Horizontal") * playerSpeed;
+    //    float forwardMovement = Input.GetAxis("Vertical") * playerSpeed;
+
+    //    transform.position += new Vector3(horizontalMovement, 0, forwardMovement);
+    //}
+
+    //void PlayerJumpMovement()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space) && PlayerGrounded())
+    //    {
+    //        print("jump pressed");
+    //        rb.AddForce(0, playerJumpValue, 0);
+    //        //rb.velocity = new Vector3(0, playerJumpValue, 0);
+    //    }
+    //}
 }
