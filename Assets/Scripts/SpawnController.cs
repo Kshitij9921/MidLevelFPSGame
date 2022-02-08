5377cd690385be4926e38f152c2f7fbb01834be1
Kshitij9921 <kshitijcobra@gmail.com>
2022-02-08 11:01:45 +0530

    zombiecontroller
    created a random zombie spawner in set radius, fixed zombies spawning in the air

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpawnController : MonoBehaviour
 {
     
+    public GameObject zombiePrefab;
+    public int number;
+    public float spawnRadius;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //we want to randomly spawn the zombies
+        for (int i = 0; i < number; i++)
+        {
+            Vector3 randomPoint = this.transform.position + Random.insideUnitSphere * spawnRadius;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 10f, NavMesh.AllAreas))
+            {
+                Instantiate(zombiePrefab, hit.position, Quaternion.identity);
+            }
+             
+        }
     }
 
     // Update is called once per frame
